4438cafe531e49df15f7605f08e74c1c51d03370
Radu Matusa <matusaradu@gmail.com>
2020-03-27 21:40:39 +0100

    Camera Done untill player be moving

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    private Transform player;
+    private Vector3 target_Offset;
+    private float cameraRotation;
+    private float cameraZoom;
+
+    private Input controls;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraRotation = transform.eulerAngles.y;
+        target_Offset = transform.position - player.position;
+        controls = new Input();
+        controls.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rotate(controls.Player.CameraRotate.ReadValue<float>());
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cameraRotation, transform.eulerAngles.z);
+        transform.position = Vector3.Lerp(transform.position, player.position + target_Offset, 0.1f);
+    }
+
+    private void Rotate(float context)
+    {
+        cameraRotation += context;
     }
 }
---
+++ Assets/Input.cs
@@ -0,0 +1,154 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Input : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Input()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Input"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""c0825111-2300-44cd-bc59-83b1c48c4006"",
+            ""actions"": [
+                {
+                    ""name"": ""Camera Rotate"",
+                    ""type"": ""Value"",
+                    ""id"": ""300e1f56-75a7-4e1b-b7e1-bf4bc3718707"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""aaebceda-186a-4ff6-978d-e962ed3c34c1"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera Rotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""04105bec-9ec2-40d2-be65-41314ec346d1"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""23fe6493-7cc9-4f38-82a2-146401294541"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_CameraRotate = m_Player.FindAction("Camera Rotate", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_CameraRotate;
+    public struct PlayerActions
+    {
+        private @Input m_Wrapper;
+        public PlayerActions(@Input wrapper) { m_Wrapper = wrapper; }
+        public InputAction @CameraRotate => m_Wrapper.m_Player_CameraRotate;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @CameraRotate.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCameraRotate;
+                @CameraRotate.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCameraRotate;
+                @CameraRotate.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCameraRotate;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @CameraRotate.started += instance.OnCameraRotate;
+                @CameraRotate.performed += instance.OnCameraRotate;
+                @CameraRotate.canceled += instance.OnCameraRotate;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnCameraRotate(InputAction.CallbackContext context);
+    }
+}
