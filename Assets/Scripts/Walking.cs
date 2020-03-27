dbe854214b41d8c5a8966a673aa6cb675055b82a
Bence Imre Kiss <kissbenceimre99@gmail.com>
2020-03-27 21:23:26 +0100

    Walking works, need to change input probably

---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Walking : MonoBehaviour
+{
+    CharacterController controller;
+
+    [SerializeField] float speed = 5f;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move() 
+    {
+        Keyboard kb = InputSystem.GetDevice<Keyboard>();
+        if (kb.wKey.isPressed)
+        {
+            controller.Move(speed * transform.forward * Time.deltaTime);
+        }
+        if (kb.sKey.isPressed)
+        {
+            controller.Move(speed * transform.forward * Time.deltaTime * -1);
+        }
+        if (kb.aKey.isPressed)
+        {
+            controller.Move(speed * transform.right * Time.deltaTime * -1);
+        }
+        if (kb.dKey.isPressed)
+        {
+            controller.Move(speed * transform.right * Time.deltaTime);
+        }
+    }
+}
